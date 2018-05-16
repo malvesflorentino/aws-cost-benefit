1f59ca08b3317bea7c934007f64e39515a8a3127
malvesflorentino <37453018+malvesflorentino@users.noreply.github.com>
2018-05-16 18:55:06 -0300

    Changing the names of variables

--- initial.py
+++ initial.py
@@ -1,36 +1,35 @@
-def cost_bennefit_inner():
+def cost_benefit_input():
     with open("1.txt", 'r') as input1:
-        lista = []
+        listed = []
         for line in input1.readlines():
             if 'HVM' in (line):
                 it = line.split(',')[0]
-                vpcu = line.split(',')[1]
+                vcpu = line.split(',')[1]
                 with open("2.txt", 'r') as input2:
                     for line1 in input2.readlines():
                         if it in (line1):
                             mem = line1.split(',')[1]
-                            custo = line1.split(',')[2]
+                            cost = line1.split(',')[2]
                             # Formula #
                             mem = float(mem)
-                            vpcu   = float(vpcu)
-                            custo = float(custo)
-                            cost_benefit = (((mem / 3.75) + vpcu)/custo)
+                            vcpu   = float(vcpu)
+                            cost = float(cost)
+                            cost_benefit = (((mem / 3.75) + vcpu)/cost)
                             cost_benefit = round(cost_benefit, 0)
                         
-                            lista.append((it,cost_benefit))
-        return lista
+                            listed.append((it,cost_benefit))
+        return listed
 
-def cost_bennefit_out(lista):
-    lista = sorted(lista, reverse=False, key=lambda it: it[1])
+def cost_benefit_output(listed):
+    listed = sorted(listed, reverse=False, key=lambda it: it[1])
     with open("output.txt", 'a') as output:
-        for i in lista:
+        for i in listed:
             i = i[0]
             output.write(i+'\n')
 def main():
-    a = cost_bennefit_inner()
-    cost_bennefit_out(a)
+    listed = cost_benefit_input()
+    cost_benefit_output(listed)
    
    
 if __name__ == "__main__":
     main()
-
